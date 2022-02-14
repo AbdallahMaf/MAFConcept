10d416b18350ef91cf80233c9a6a1a585af8ef66
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-02-14 01:31:38 +0000

    commit nbr3 navbar-accueil

---
+++ name.js
@@ -0,0 +1,14 @@
+let i = 0;
+let text3 = "Web & Mobile Full-Stack Junior";
+function progress(){
+    if(i<text3.length){
+        document.querySelector("#text3").innerHTML += text3.charAt(i);
+        i++;
+
+        setTimeout(progress, 300);
+        // if(i==16){
+        //     document.querySelector("#text3").innerHTML += "</br>";
+        // }
+    }
+}
+progress();
