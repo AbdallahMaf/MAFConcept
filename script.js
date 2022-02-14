2d56c7f74726d055c30ee96b2fba8cf5594ce594
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-02-14 16:04:45 +0000

    commit nbr5 QSJ

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY >20){
             $('.navbar').addClass("sticky");
-            $('.middle').addClass("sticky");
+            //$('.middle').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
-            $('.middle').removeClass("sticky");
+            //$('.middle').removeClass("sticky");
         }
     });
 
