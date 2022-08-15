0d6b2d5c13e0b71481e0f959486c9ae8e1e38895
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-08-15 20:37:15 +0000

    studentcomputerScienceJS

--- name.js
+++ name.js
@@ -6,9 +6,20 @@ function progress(){
         i++;
 
         setTimeout(progress, 300);
-        if(i==23){
-            document.querySelector("#text3").innerHTML += "<br>";
-        }
+        // if(i==23){
+        //     document.querySelector("#text3").innerHTML += "<br>";
+        // }
     }
 }
 progress();
+
+let j = 0;
+let text4 = "Informatique";
+function progress2(){
+    if(j<text4.length) {
+        document.querySelector("#text4").innerHTML += text4.charAt(j);
+        j++;
+        setTimeout(progress2, 400);
+    }
+}
+progress2();
