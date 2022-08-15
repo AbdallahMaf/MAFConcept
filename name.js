801c0f5b7cea4c73ff142a56fe9fb17b5165b80f
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-08-15 18:47:23 +0000

    Ingenieur

--- name.js
+++ name.js
@@ -1,14 +1,14 @@
 let i = 0;
-let text3 = "Web & Mobile Full-Stack";
+let text3 = "Web & Mobile Full-Stack Eleve Ingenieur en Informatique";
 function progress(){
-    if(i<text3.length){
+    if(i<text3.length) {
         document.querySelector("#text3").innerHTML += text3.charAt(i);
         i++;
 
         setTimeout(progress, 300);
-        // if(i==16){
-        //     document.querySelector("#text3").innerHTML += "</br>";
-        // }
+        if(i==23){
+            document.querySelector("#text3").innerHTML += "<br>";
+        }
     }
 }
 progress();
