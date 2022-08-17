39e954548f2652c28856443de30ba35cc8b9dd15
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-08-17 20:38:09 +0000

    ViewModalJS

---
+++ modal.js
@@ -0,0 +1,23 @@
+bg= document.getElementsByClassName("bg");
+
+document.querySelectorAll(".contact-link").forEach(item =>{
+    item.addEventListener("click", event =>{
+
+        id = item.getAttribute("data-productid");
+
+        contactCV = document.getElementById( id );
+        bg[0].style.display = "block";
+
+        contactCV.style.top = "160%"
+    })
+})
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "Alt"){
+       bg[0].style.display = "none";
+    
+        document.querySelectorAll(".contact").forEach(contactCV =>{
+            contactCV.style.top = "-50%";
+        })
+    }
+})
