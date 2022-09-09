f38323b2c8f19513c2a960ece8898ed7bb15c503
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-09-09 03:19:34 +0000

    typinAnimationjs

--- name.js
+++ name.js
@@ -14,31 +14,31 @@
 //     TL.play();
 // })
 
-let i = 0;
-let text3 = "Web & Mobile Full-Stack";
-function progress(){
-    if(i<text3.length) {
-        document.querySelector("#text3").innerHTML += text3.charAt(i);
-        i++;
+// let i = 0;
+// let text3 = "Web & Mobile Full-Stack";
+// function progress(){
+//     if(i<text3.length) {
+//         document.querySelector("#text3").innerHTML += text3.charAt(i);
+//         i++;
 
-        setTimeout(progress, 300);
-        // if(i==23){
-        //     document.querySelector("#text3").innerHTML += "<br>";
-        // }
-    }
-}
-progress();
+//         setTimeout(progress, 300);
+//         // if(i==23){
+//         //     document.querySelector("#text3").innerHTML += "<br>";
+//         // }
+//     }
+// }
+// progress();
 
-let j = 0;
-let text4 = "</Informatique>";
-function progress2(){
-    if(j<text4.length) {
-        document.querySelector("#text4").innerHTML += text4.charAt(j);
-        j++;
-        setTimeout(progress2, 400);
-    }
-}
-progress2();
+// let j = 0;
+// let text4 = "</Informatique>";
+// function progress2(){
+//     if(j<text4.length) {
+//         document.querySelector("#text4").innerHTML += text4.charAt(j);
+//         j++;
+//         setTimeout(progress2, 400);
+//     }
+// }
+// progress2();
 
 let k = 0;
 let text5 = "Web & Mobile Full-Stack Junior et Integrateur Systeme d'Information";
@@ -46,6 +46,7 @@ function progress3(){
     if(k<text5.length) {
         document.querySelector("#text5").innerHTML += text5.charAt(k);
         k++;
+        
 
         setTimeout(progress3, 300);
         if(k==23){
@@ -55,20 +56,42 @@ function progress3(){
 }
 progress3();
 
-let l = 0;
-let text6 = "sportif...";
-function progress4(){
-    if(l<text6.length) {
-        document.querySelector("#text6").innerHTML += text6.charAt(l);
-        l++;
+// let l = 0;
+// let text6 = "sportif...";
+// function progress4(){
+//     if(l<text6.length) {
+//         document.querySelector("#text6").innerHTML += text6.charAt(l);
+//         l++;
 
-        setTimeout(progress4, 300);
-        if(l==23){
-            document.querySelector("#text6").innerHTML += "<br>";
-        }
-    }
-}
-progress4();
+//         setTimeout(progress4, 300);
+//         if(l==23){
+//             document.querySelector("#text6").innerHTML += "<br>";
+//         }
+//     }
+// }
+//progress4();
 
 
+//A jQuery typing animation script
+let typed = new Typed("#text3", {
+    strings: ["Web et Mobile Full-Stack"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
 
+let typedinf = new Typed("#text4", {
+    strings: ["Informatique"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
+});
+
+let typedKarate = new Typed("#text6", {
+    strings: ["Sportif..."],
+    typeSpeed: 260,
+    backSpeed: 260,
+    loop: true
+
+});
