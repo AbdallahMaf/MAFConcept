e2ff23001386b1e892235fe71f59e83946f18159
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-11-13 19:06:10 +0000

    ScrollRevealCompetencesTop

--- name.js
+++ name.js
@@ -46,7 +46,7 @@ sr.reveal('.home-content',{delay:190, origin:'bottom'})
 sr.reveal('.qsj .qsj-content .left',{delay:200, origin:'top', distance: '150px'})
 sr.reveal('.qsj .qsj-content .right',{delay:200, origin:'bottom', distance: '150px'})
 
-sr.reveal('.competences .text-1',{delay:200, origin:'right', distance:'100px'})
+sr.reveal('.competences .text-1',{delay:200, origin:'top', distance:'70px'})
 
 
 
